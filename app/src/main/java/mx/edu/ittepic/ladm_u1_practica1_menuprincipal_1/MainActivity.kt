e0a999f9fa20a3aff545ddebcc114944144b9392
ArletheMora <armomorali@ittepic.edu.mx>
2022-09-10 20:45:59 -0600

    Se modificó algo de diseño y se agregó toast

--- app/src/main/java/mx/edu/ittepic/ladm_u1_practica1_menuprincipal_1/MainActivity.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u1_practica1_menuprincipal_1/MainActivity.kt
@@ -1,11 +1,29 @@
 package mx.edu.ittepic.ladm_u1_practica1_menuprincipal_1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import mx.edu.ittepic.ladm_u1_practica1_menuprincipal_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lista.setOnClickListener {
+            //El activity 2 lo guardamos en una variable
+            var otroActivity = Intent(this, MainActivity2::class.java)
+            startActivity(otroActivity) //Activar
+        }
+
+        binding.agregar.setOnClickListener {
+            //Mandar mensaje de que se guardo
+            Toast.makeText(this, "Se agrego el producto", Toast.LENGTH_LONG)
+                .show()
+        }
     }
 }
